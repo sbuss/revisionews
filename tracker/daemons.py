41580c9451c07e0bf8f94796346d49d4610fc513
sbuss <steven@revisionews.com>
2011-03-21 01:28:49 +0000

    Added a proper Daemon class which actually forks away from the shell. FeedReaderDaemon now monitors all feeds by default

--- tracker/daemons.py
+++ tracker/daemons.py
@@ -1,10 +1,145 @@
+#!/usr/bin/env python
 from __future__ import division
 
+import sys, os, time, atexit
 import argparse
 import datetime
 import time
+from signal import SIGTERM 
 
-class PeriodicDaemon(object):
+class Daemon(object):
+    """A generic daemon class.
+
+    Daemon class from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
+    This is written by Sander Marechal and was obtained from his website at 
+    http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
+    It has been slightly modified to work with new-style classes
+    
+    Usage: subclass the Daemon class and override the run() method
+    """
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+        super(Daemon, self).__init__()
+    
+    def daemonize(self):
+        """
+        do the UNIX double-fork magic, see Stevens' "Advanced 
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try: 
+            pid = os.fork() 
+            if pid > 0:
+                # exit first parent
+                sys.exit(0) 
+        except OSError, e: 
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+    
+        # decouple from parent environment
+        os.chdir("/") 
+        os.setsid() 
+        os.umask(0) 
+    
+        # do second fork
+        try: 
+            pid = os.fork() 
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0) 
+        except OSError, e: 
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1) 
+    
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+    
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile,'w+').write("%s\n" % pid)
+    
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError as e:
+            pid = None
+    
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+        
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+
+        # Try killing the daemon process    
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
+
+    def run(self):
+        """
+        You should override this method when you subclass Daemon. It will be called after the process has been
+        daemonized by start() or restart().
+        """
+
+class PeriodicDaemon(Daemon):
     parser = argparse.ArgumentParser(add_help=False, # argparse issue
                                      description="Monitor an SQS queue.")
     parser.add_argument("-max", action="store", dest="max_wait", 
@@ -13,8 +148,10 @@ class PeriodicDaemon(object):
                         default=500, type=int, help="The minimum number of milliseconds to wait between runs, defaults to 500 (0.5 sec)")
     parser.add_argument("-log", action="store", dest="log_level",
                         default="INFO", help="Log level (INFO or DEBUG)")
+    parser.add_argument("command", help="Command for the daemon: start, stop, or restart")
 
-    def __init__(self, max_wait, min_wait, log=None):
+    def __init__(self, max_wait, min_wait, log=None, 
+                  *args, **kwargs):
         """Initializes a PeriodicDaemon.
         .. function: __init__(, max_wait, min_wait[, log])
 
@@ -27,6 +164,7 @@ class PeriodicDaemon(object):
         self.max_wait = max_wait
         self.min_wait = min_wait
         self.current_wait = min_wait
+        super(PeriodicDaemon, self).__init__(*args, **kwargs)
 
     def reset_timer(self):
         self.last_access = datetime.datetime.now()
@@ -60,6 +198,7 @@ class PeriodicDaemon(object):
         If you want the least amount of effort, just implement a do_work method
         and the Daemon will periodically run it for you, with exponential 
         backoff and fast starting."""
+        self.log.info("Entering run loop")
         while True:
             success = self.do_work()
             self.reset_timer() # Update timer with time of last run
@@ -90,13 +229,15 @@ class SQSDaemon(PeriodicDaemon):
     parser.add_argument("-queue", help="Name of the queue to process. This should correspond to a named feed from the feeds.txt file. ")
     """Daemon that periodically checks an SQS queue.
     """
-    def __init__(self, queue_name, *args, **kwargs):
+    def __init__(self, queue_name, pidfile="/tmp/sqs_daemon_%s.pid", 
+                 *args, **kwargs):
         """Initializes the Processor daemon.
         .. function: __init__(queue_name, *args, **kwargs)
 
         :param queue_name: The name of the queue, corresponding to an entry in feeds.txt
         """
-        super(self, SQSDaemon).__init__(*args, **kwargs)
+        super(SQSDaemon, self).__init__(pidfile=(pidfile % queuename), 
+                                        *args, **kwargs)
         self.q = sqs_utils.get_queue(queue_name)
         self.log.debug("Queue is %s" % self.q)
 
--- tracker/feed_reader.py
+++ tracker/feed_reader.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import logging
 import logging.config
 import operator
+import sys
 
 import feedparser
 
@@ -13,7 +14,7 @@ from tracker import feed_utils
 from tracker import sqs_utils
 
 # File to store the timestamp of the last feed read
-LAST_ACCESS = ".last-access.%s"
+LAST_ACCESS = "/tmp/feed_reader.last-access.%s"
 
 # Configure logger
 logging.config.fileConfig("logging.conf")
@@ -22,17 +23,31 @@ log.level = logging.DEBUG
 
 class FeedReaderDaemon(PeriodicDaemon):
     """Daemon for reading RSS feeds."""
-    def __init__(self, feed, *args, **kwargs):
+    def __init__(self, feed_readers, *args, **kwargs):
         """Initialize the FeedReaderDaemon.
+        :type feed_readers: :py:`list` of :class:`tracker.feed_reader.FeedReader`
+        :param feed_readers: A :py:`list` of :class:`tracker.feed_reader.FeedReader`s to run
+        """
+        self.feed_readers = feed_readers
+        super(FeedReaderDaemon, self).__init__(
+                    pidfile="/tmp/feed_reader_daemon.pid", *args, **kwargs)
 
+    def do_work(self):
+        sys.stdout.write("in do_work\n")
+        for feed in self.feed_readers:
+            feed.run()
+
+class FeedReader(object):
+    def __init__(self, feed, log):
+        """
         :type feed: :class:`tracker.feed_utils.Feed`
-        :param feed: The :class:`~tracker.feed_utils.Feed` to monitor."""
+        :param feed: A :class:`~tracker.feed_utils.Feed` to monitor."""
         self.feed = feed
         self.queue = sqs_utils.get_queue(feed.domain)
         self.dateformat = "%Y-%m-%dT%H:%M:%SZ"
-        super(FeedReaderDaemon, self).__init__(*args, **kwargs)
+        self.log = log
 
-    def do_work(self):
+    def run(self):
         new_items = 0
         # Get the feed
         f = feedparser.parse(self.feed.url)
@@ -53,11 +68,11 @@ class FeedReaderDaemon(PeriodicDaemon):
                 break
 
         if new_items == 0:
-            self.log.info("No work to do for %s" % feed.domain)
+            self.log.info("No work to do for %s" % self.feed.domain)
         else:
-            self.log.info("Added %s new items to %s" % (new_items, feed.domain))
+            self.log.info("Added %s new items to %s" % (new_items, self.feed.domain))
         self.log.info("Setting last access for %s as %s" % \
-                        (feed.domain, sorted_entries[0].updated))
+                        (self.feed.domain, sorted_entries[0].updated))
         self.update_last_access(sorted_entries[0].updated)
 
     def add_to_queue(self, entry):
@@ -80,9 +95,13 @@ class FeedReaderDaemon(PeriodicDaemon):
     
     def get_last_access(self):
         # Get the last access time
-        f = open(LAST_ACCESS % self.feed.domain , 'r')
-        last_access = datetime.strptime(f.readline().strip(), self.dateformat)
-        f.close()
+        last_access = datetime.now()
+        try:
+            f = open(LAST_ACCESS % self.feed.domain , 'r')
+            last_access = datetime.strptime(f.readline().strip(), self.dateformat)
+            f.close()
+        except IOError:
+            self.log.warn("Missing %s" % (LAST_ACCESS % self.feed.domain))
         return last_access
 
     def update_last_access(self, last_access_time):
@@ -92,23 +111,10 @@ class FeedReaderDaemon(PeriodicDaemon):
         f.write(last_access_time)
         f.close()
 
-
-def start(feed, max_wait, min_wait):
-    log.info("Starting feed reader daemon with feed %s" % feed.domain)
-    frd = FeedReaderDaemon(feed = feed, 
-                           max_wait = max_wait, 
-                           min_wait = min_wait, 
-                           log = log)
-    frd.run()
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download RSS feeds",
                                      parents=[PeriodicDaemon.parser],
                                      conflict_handler="resolve")
-    parser.add_argument("-feed", dest="feed_name",
-                        default="all", help="Name of the feed to track, \
-                                        corresponding with a feed name in \
-                                        feeds.txt.")
     parser.add_argument("-max", action="store", dest="max_wait", 
                         default=(1000 * 60 * 5), # 5 minutes
                         type=int, help="The maximum number of milliseconds to wait between runs, defaults to 5 minutes")
@@ -116,14 +122,35 @@ if __name__ == "__main__":
                         default=(1000 * 60 * 5), type=int, help="The minimum number of milliseconds to wait between runs, defaults to 5 minutes")
     args = parser.parse_args()
     feeds = feed_utils.get_feeds()
-    feed = None
-    if args.feed_name not in feeds:
-        raise Exception("Unknown feed name: %s" % args.feed_name)
-    else:
-        feed = feeds[args.feed_name]
+    readers = []
+    for feed in feeds.itervalues():
+        readers.append(FeedReader(feed, log))
     # Lower the logging verbosity, since this is probably run as a daemon
     log.level = logging.INFO
     if args.log_level == "DEBUG":
         log.level = logging.DEBUG
-    start(feed, args.max_wait, args.min_wait)
+    frd = FeedReaderDaemon(feed_readers = readers, 
+                           max_wait = args.max_wait, 
+                           min_wait = args.min_wait, 
+                           log = log)
+    if args.command == "start":
+        log.info("Starting feed reader daemon with feeds %s" % \
+            ",".join([feed for feed in feeds.iterkeys()]))
+        frd.start()
+        log.info("Started")
+        sys.exit(0)
+    elif args.command == "stop":
+        log.info("Stopping feed reader daemon with feeds %s" % \
+            ",".join([feed for feed in feeds.iterkeys()]))
+        frd.stop()
+        sys.exit(0)
+    elif args.command == "restart":
+        log.info("Restarting feed reader daemon with feeds %s" % \
+            ",".join([feed for feed in feeds.iterkeys()]))
+        frd.restart()
+        sys.exit(0)
+    else:
+        print("Invalid command. Expected 'start', 'stop', or 'restart'.")
+        parser.print_usage()
+        sys.exit(2)
 
